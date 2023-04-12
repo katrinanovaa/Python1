cd2e232a66bd817af1b0ed6a777daf00a2a30f53
Катерина Андреевна <katrina_nova@mail.ru>
2023-04-12 20:56:54 +0500

    1 задача (34)  7 семинара

--- Home_7/Sem7/Sem7_1.py
+++
@@ -1,6 +0,0 @@
-file = open('data.txt', mode = 'r', encoding = 'UTF-8')
-
-for el in file:
-    print(el)
-    
-file.close()       #если не закрыть, то может файл остаться не закрытым и помешать дальнейшей работе ОС
